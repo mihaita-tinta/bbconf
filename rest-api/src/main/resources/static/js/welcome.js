ee130a7c58b1446eed52b23bae4e015e0fc390fa
Mihaita Tinta <mihaita.tinta@ing.com>
2019-11-13 06:53:15 +0200

    get ready for conf

--- rest-api/src/main/resources/static/js/welcome.js
+++ rest-api/src/main/resources/static/js/welcome.js
@@ -15,43 +15,11 @@ function start() {
 }
 
 function buildIcon(myCustomColour) {
-    var lat = 5.5,
-        lon = 5.5;
-
-    var caption = '', // '<i class="fa fa-eye" />' or 'abc' or ...
-        size = 10,    // size of the marker
-        border = 2;   // border thickness
-
-    var markerHtmlStyles = ' \
-    background-color: ' + myCustomColour + '; \
-    width: '+ (size * 3) +'px; \
-    height: '+ (size * 3) +'px; \
-    display: block; \
-    left: '+ (size * -1.5) +'px; \
-    top: '+ (size * -1.5) +'px; \
-    position: relative; \
-    border-radius: '+ (size * 3) +'px '+ (size * 3) +'px 0; \
-    transform: rotate(45deg); \
-    border: '+border+'px solid #FFFFFF;\
-    ';
-
-    var captionStyles = '\
-    transform: rotate(-45deg); \
-    display:block; \
-    width: '+ (size * 3) +'px; \
-    text-align: center; \
-    line-height: '+ (size * 3) +'px; \
-    ';
 
     return L.divIcon({
-        className: "color-pin-" + myCustomColour.replace('#', ''),
-
-        // on another project this is needed: [0, size*2 + border/2]
-        iconAnchor: [border, size*2 + border*2],
-
-        labelAnchor: [-(size/2), 0],
-        popupAnchor: [0, -(size*3 + border)],
-        html: '<span style="' + markerHtmlStyles + '"><span style="'+captionStyles+'">'+ caption + '</span></span>'
+        className: 'custom-div-icon',
+        html: "<div style='background-color:" + myCustomColour + ";' class='marker-pin'></div><i class='material-icons'>face</i>",
+        iconSize: [30, 42],
     });
 }
 
